2452cc3c2bb5781728b03d4c92a9f8b3fc3ba2a9
CasFaase2003 <132989642+CasFaase2003@users.noreply.github.com>
2023-05-09 10:01:29 +0200

    Hele Project toevoegen

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>Taxi Op Weg</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="CSS/css.css">
+        <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script src="Javascript/JS.js"></script>
+    </head>
+    <body>
+
+    <div class="topnav" id="myTopnav">
+    <img src="Foto's/logo.png" width="120px"> 
+      <a href="#Contact">Contact</a>
+      <a href="#Mij">Over Mij</a>
+      <a href="#Tijden">Openingstijden</a>
+      <a href="#Tarieven">Tarieven</a>
+      <a href="#Home">Home</a>
+      <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+    </div>
+
+        <p>
+            <img class="midden" src="Foto's/foto_taxi_bij_zeelandbrug.JPG" width= 600px>
+        </p>
+
+        <div class="text" id="Home">
+        <h1>Bel Taxi Op Weg: <a href="tel:+31614557450" class="telefoon"> 06-14557450 </a><span class="material-symbols-outlined">call</span></h1>
+            <h2>Welkom bij Taxi Op Weg!</h2>
+            <b>Taxi Op Weg</b> verzorgt veelzijdig vervoer vanuit Goes. Privé of zakelijk. Dichtbij of ver weg. Naar alle windstreken.
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                    Particulier Vervoer
+                    </div>
+                    <div class="col">
+                    Zakelijk Vervoer
+                    </div>
+                    <div class="col">
+                    Contact Vervoer
+                    </div>
+                    <div class="col">
+                    Luchthavenvervoer
+                    </div>
+                    <div class="col">
+                    Pakketvervoer
+                    </div>
+                </div>
+            </div>
+            
+            Meer Informatie?
+            Bel dan <a href="tel:+31614557450" class="telefoon"> 06-14557450</a><span class="material-symbols-outlined">call</span>, of mail naar <a href="mailto:info%40taxiopweg%2Enl" title="Contact met Taxi Op Weg" target="_blank"> info@taxiopweg.nl</a><span class="material-symbols-outlined">mail</span>
+        </div>
+
+        <br>
+        <br>
+			<p>
+				<table style="float:right" width="30%">
+                    <tr>
+                        <td><h2>Tariefopbouw</h2></td>
+                    </tr>
+
+					<tr>
+						<td>Starttarief:</td>
+						<td>€ 3,60</td>
+					</tr>
+					<tr>
+						<td>Kilometertarief:</td>
+						<td>€ 2,65</td>
+					</tr>
+					<tr>
+						<td>Minuuttarief:</td>
+						<td>€ 0,44</td>
+					</tr>
+					<tr>
+						<td>Wachttarief:</td>
+						<td>€ 45,00 per uur</td>
+					</tr>
+					<tr>
+						<td colspan="2"><strong>Alle tarieven zijn inclusief 9% BTW.</strong></td>
+					</tr>
+				</table>
+			</p>
+
+            <div id="Tarieven">
+            <h2>Tarieven</h2>
+            Taxi Op Weg hanteert de <a href="https://www.rijksoverheid.nl/onderwerpen/taxi/vraag-en-antwoord/wat-zijn-de-kosten-voor-een-taxi" title="Maximum taxitarieven vanuit de Rijksoverheid" target="_blank">maximumtarieven</a> zoals de Rijksoverheid die heeft vastgesteld.
+            <br>
+            Wat dat betekent voor de ritprijs is te zien in de onderstaande twee rekenvoorbeelden:
+            <br>
+            - Een stadsrit in Goes, bijvoorbeeld 3 Km in 8 minuten, kost €15,10.
+            <br>
+            - Een snelwegrit van Goes naar Vlissingen, bijvoorbeeld 29 Km in 28 minuten, kost €92,80.
+            <br>
+            <br>
+            <h2>Meer Informatie</h2>
+            Wilt u meer informatie over tarieven? Bijvoorbeeld over kortingen op luchthavenvervoer? Of prijzen voor contractvervoer?
+            <br>
+            Neem vrijblijvend contact op via <a href="tel:+31614557450" class="telefoon">06-14557450</a><span class="material-symbols-outlined">call</span>.
+        </div>
+        <br>
+
+        <div id="Tijden">
+        <h2>Openingstijden</h2>
+			Taxi Op Weg is een zzp-onderneming met één taxi en één chauffeur. De beschikbaarheid is afhankelijk van de vraag. 
+            Plaatsen en tijden waar(op) u Taxi Op Weg kunt vinden zijn het station van Goes op door-de-weekse dagen of de Grote Markt in Goes op vrijdag- en zaterdagnacht. Om zeker van een taxi te zijn kunt u beter van te voren bellen naar <a href="tel:+31614557450" class="telefoon">06-14557450</a><span class="material-symbols-outlined">call</span>
+            <br>
+            <br>
+            <div class="open">
+                <br>
+                <b>Taxi Op Weg is bereikbaar.</b>
+                <br>
+            </div>
+            <br>
+            Als de <a href="https://www.ilent.nl/onderwerpen/taxiondernemers/wet-en-regelgeving" title="Rij- en rusttijden taxi's vanuit de Rijksoverheid" target="_blank">rij- en rusttijdenwet van de overheid</a> voorschrijft dat er niet mag worden gereden of als er andere redenen zijn waardoor Taxi Op Weg niet onderweg kan zijn dan staat er een telefonische afwezigheidsmelding ingesteld die verwijst naar de openingstijdenpagina van Taxi Op Weg</a> waar staat vermeld vanaf welke dag en welk tijdstip Taxi Op Weg weer op weg is.
+        </div>
+        <br>
+
+        <table style="float:right" width="15%">
+        <tr>
+            <td>
+                <img src="Foto's/tony_vd_berg.jpg" width="175px;">
+            </td>
+        </tr>
+    </table>
+
+        <div id="Mij">
+            <h2>Over Mij</h2>
+            <strong>Taxi Op Weg</strong> is door mij opgericht omdat die ene zonnige dag achter het stuur van een taxi, eind jaren negentig, in <a href="https://www.taxi13000.nl/" title="Homepage van Taxi13000 te Haarlem" target="_blank">Haarlem</a>, in mijn hoofd bleef hangen. 
+            <br>Ik studeerde scheepsbouwkunde, maar ik vond het taxiwerk zo leuk, zeker die dag, dat ik het wel heel mijn leven zou kunnen doen in plaats van tijdelijk. 
+            <br>Maar goed, dat deed ik natuurlijk niet. Ik studeerde af en ging werken. Werken op een kantoor. Maar ik miste wat, schoolde me om, 
+            <br>werd leraar en verruilde het kantoor voor het klaslokaal. Een hele verbetering. Maar... ik miste wat... en stapte in 2018 voor <a href="https://www.taxisteketee.nl/" title="Homepage van Taxi Steketee" target="_blank">Taxi Steketee</a> weer in een taxi als taxichauffeur. 
+            <br>Toch miste ik nog steeds wat: vrijheid. 
+            Na veel wikken en wegen, terugdenkend aan die ene dag in Haarlem en de afgelopen 25 jaar overziend, heb ik uiteindelijk de knoop doorgehakt en een eigen taxi gekocht. Het resultaat is <strong>Taxi Op Weg</strong>. Mijn beste beslissing ooit.
+            <br><br>
+            Van harte welkom aan boord!
+            <br>Tony van de Berg
+        </div>
+        <br>
+
+        <div id="Contact" class="text">
+          <h2>Contact</h2>
+          Bel als u een lift ergens naar toe wilt naar:<a href="tel:+31614557450" class="telefoon"><h1>06-14557450<span class="material-symbols-outlined">call</span></h1></a>
+        </div>
+    </body>
+    <br>
+
+    <footer class="text">
+    ©Taxi Op Weg, 2023. Alle rechten voorbehouden. <a href="Documenten/Algemene Voorwaarden Taxi Op Weg 13_juni_2022.pdf" title="Algemene Voorwaarden van Taxi Op Weg" target="_blank"><strong>Algemene Voorwaarden</strong></a> zijn van toepassing.
+    </footer>
+</html>
